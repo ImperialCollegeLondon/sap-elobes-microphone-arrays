f9d548c9445f7ec1093d8bc594facedfe338df83
Alastair Moore <alastair.h.moore@imperial.ac.uk>
2022-01-27 10:38:02 +0000

    Added co-ordinates swapping to match our convention

--- FreeField_202201125_EasyComFramesOnly.m
+++ FreeField_202201125_EasyComFramesOnly.m
@@ -23,9 +23,24 @@ function[sensor_pos] = predefinedSensorPositions()
 % evaluate to determine the postitions of the elements relative to
 % the origin
 
-sensor_pos = 1e-3 .* [... % [x,y,z] offsets of sensors
-        82, -5, -29;...
-        -1, -1,  30;...
-       -77, -2,  11;...
-       -83, -5, -60];
+% FRL's AR glass sensor positions
+% taken from https://github.com/facebookresearch/EasyComDataset
+
+% Note the different coordiaten system
+% Ours (here)   |    Theirs (FRL)
+% ---------------------------------
+%       X       |       Z       
+%       Y       |       X       
+%       Z       |       Y       
+
+% their given xyz (in mm) - yzx in our format
+sensor_pos = [...
+    82 -5 -29;...
+    -1 -1 30;...
+    -77 -2 11;...
+    -83 -5 -60;...
+    ];
+
+sensor_pos=sensor_pos(:,[3 1 2]); % yzx to xyz
+sensor_pos = sensor_pos *1e-3; % in m
 end
